b79a37863729e74f6ae617a56c0bba0aafe27163
Fernando Ramirez <fernando.ramirez117@yahoo.com>
2023-04-23 12:26:49 -0500

    updated colorChange

--- Assets/Scripts/colorChange.cs
+++ Assets/Scripts/colorChange.cs
@@ -5,17 +5,54 @@ using UnityEngine.UI;
 
 public class colorChange : MonoBehaviour
 {
-    public Image bg;
+    public Image materialToChange;
     // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     bg.color =  Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+        
+    // }
+
+    public float duration;
+    //public Material materialToChange;
+    public Color targetColor = new Color(0,1,0,1);
+    public Color targetColor2 = new Color(0,1,0,1);
     void Start()
     {
-        
+        //materialToChange = gameObject.GetComponent<Renderer>().material;
+        StartCoroutine(LerpFunction(Color.blue, duration));
+    }
+    IEnumerator LerpFunction(Color endValue, float duration)
+    {
+        float time = 0;
+        Color startValue = materialToChange.color;
+        while (time < duration)
+        {
+            materialToChange.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        materialToChange.color = endValue;
+        StartCoroutine(LerpFunction2(Color.red, duration));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LerpFunction2(Color endValue, float duration)
     {
-        bg.color =  Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
-        
+        float time = 0;
+        Color startValue = materialToChange.color;
+        while (time < duration)
+        {
+            materialToChange.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        materialToChange.color = endValue;
+        StartCoroutine(LerpFunction(Color.blue, duration));
     }
 }
